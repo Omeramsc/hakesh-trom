5b89a32da09101d9f20e2c1d738a228c29647c6b
OmerAmsc <Omeramsc@gmail.com>
2020-03-20 19:21:38 +0200

    moved php project to python flask

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def home():
+    return render_template('/index.html')
+
+
+@app.route('/first_steps')
+def first_steps():
+    return render_template('/first_steps.html')
+
+
+@app.route('/donation', methods=['GET', 'POST'])
+def donation():
+    # if request.method == 'GET':
+    # INSERT INTO DB / MOVE TO PP/bit
+    # return render_template('/<---->.html')
+    return render_template('/donation.html')
+
+
+@app.route('/reports', methods=['GET', 'POST'])
+def reports():
+    # GET REPORTS FROM DB
+    return render_template('/reports.html')
+
+
+@app.route('/create_report', methods=['GET', 'POST'])
+def create_report():
+    # if request.method == 'GET':
+    # INSERT INTO DB
+    # return redirect(url_for('report'))
+    return render_template('/create_report.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
