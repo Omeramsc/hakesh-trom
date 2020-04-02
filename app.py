e273b560b492c21efbb92d0d0c7b3fd04dc49148
OmerAmsc <Omeramsc@gmail.com>
2020-04-02 23:22:10 +0300

    Manage Campaign is functional with route and search

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import render_template, jsonify, flash, redirect, url_for
 from app_init import app
-from forms import CreateCampaignForm
+from forms import CreateCampaignForm, SearchCampaignForm
 from models import Campaign
 from db import db
+import datetime
+
+
+def get_icon(d1):
+    d2 = datetime.date.today()
+    if d1 > d2:
+        return "static/didnt_begin.png"
+    elif d2 > d1:
+        return "static/done.png"
+    return "static/in_progress.png"
 
 
 
@@ -40,13 +50,35 @@ def create_campaign():
     return render_template('/create_campaign.html', form=form)
 
 
-@app.route('/manage_campaign')
+@app.route('/manage_campaign', methods=['GET', 'POST'])
 def manage_campaign():
-    return render_template('/manage_campaign.html')
+    form = SearchCampaignForm()
+    campaigns = Campaign.query.all()
+    if form.submit():
+        if form.name.data:
+            if form.city.data:
+                campaigns = Campaign.query.filter(Campaign.name.like('%' + form.name.data + '%'),
+                                                  Campaign.city == form.city.data).all()
+            else:
+                campaigns = Campaign.query.filter(Campaign.name.like('%' + form.name.data + '%')).all()
+        elif form.city.data:
+            campaigns = Campaign.query.filter(Campaign.city == form.city.data).all()
+        elif form.status.data:  # For some reason, this statement never works. it is always true. and so,
+            # It's impossible to clear selection.
+            today = datetime.date.today()
+            if form.status.data == "present":
+                campaigns = Campaign.query.filter(Campaign.start_date == today)
+            elif form.status.data == "past":
+                campaigns = Campaign.query.filter(Campaign.start_date < today)
+            elif form.status.data == "future":
+                campaigns = Campaign.query.filter(Campaign.start_date > today)
+
+    return render_template('/manage_campaign.html', campaigns=campaigns, func=get_icon, form=form)
 
 
-@app.route('/manage_campaign/campaign_control_panel')
-def campaign_control_panel():
+@app.route('/manage_campaign/campaign_control_panel/<int:campaign_id>')
+def campaign_control_panel(campaign_id):
+    campaign = Campaign.query.get_or_404(campaign_id)
     return render_template('/campaign_control_panel.html')
 
 
